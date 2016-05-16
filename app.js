441c44a9e01c78d2419fe6cc8efb165b9011138d
Norvin Yecla <ngydev@outlook.com>
2016-05-16 12:43:31 +0800

    Add shortlink generating function

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 /* first page to send */
 app.get('/', function (req, res){
   fs.readFile('form.html', function(err, data){
-    res.writeHead(200, {'Content-Type': 'application/x-www-form-urlencoded', 'Content-Length': data.length });
+    res.writeHead(200, {'Content-Type': 'text/html', 'Content-Length': data.length });
     res.write(data);
     res.end();
   });
@@ -18,9 +18,21 @@ app.get('/', function (req, res){
 // POST http://localhost:8080/add
 // parameters sent with
 app.post('/add', function(req, res){
-  var url = req.body;
-  console.log(req.body);
-  res.send('this is the original url ' + url);
+  var url = req.body.url;
+  do {
+    console.log('generating link');
+    shortLink = generateLink(url);
+
+  } while (hasADuplicate(shortLink));
+
+  var MongoClient = require('mongodb').MongoClient;
+  MongoClient.connect("mongodb://localhost:27017/iklico", function(err, db) {
+    db.collection("shortlinks").insert({url: url, shortcut: shortLink});
+    console.log('done adding the url to the db');
+  });
+
+  console.log(shortLink);
+  res.send('this is the shortened url: ' + shortLink + '<br/> this is the original url' + url);
 });
 
 /* forward to corresponding URL*/
@@ -32,18 +44,53 @@ app.get('/apples/:link', function(req, res){
     db.collection("shortlinks").find({shortcut: shortLink}, function(err, docs) {
     docs.each(function(err, doc) {
         if(doc) {
+          res.send('original URL is '+shortLink);
           console.log(doc);
-          res.redirect(301, 'http://' + doc.url);
+          //res.redirect(301, 'http://' + doc.url);
         }
         else {
           res.end();
         }
       });
-    });      
-  });      
+    });
+  });
 });
 
 app.listen(3000, function(){
   console.log("Example app listening on port 3000!");
 });
+
+function generateLink(url){
+  var chars = '';
+  alpha = "abc";
+  nums = "23456789"
+  for (i = 0; i < 2; i++){
+    chars += alpha.charAt(Math.floor(Math.random()*alpha.length));
+  }
+
+  // for (i = 0; i < 2; i++){
+  //   chars += nums.charAt(Math.floor(Math.random()*nums.length));
+  // }
+
+  return chars;
+
+}
+
+function hasADuplicate(shortLink){
+  var MongoClient = require('mongodb').MongoClient;
+  MongoClient.connect("mongodb://localhost:27017/iklico", function(err, db) {
+    db.collection("shortlinks").find({shortcut: shortLink}, function(err, docs) {
+      if (docs.count() > 0) {
+        console.log("has a duplicate.");
+        return true;
+      }
+      else {
+        console.log(docs);
+        return false;
+      }
+    });
+  });
+}
+
+
 
