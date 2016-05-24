20f7214d263c02c665edef951104118df7c88149
Norvin Yecla <ngydev@outlook.com>
2016-05-24 13:42:03 +0800

    Add required field in form.ejs

--- app.js
+++ app.js
@@ -2,22 +2,24 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 
+
+
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
+
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+
 /* first page to send */
 app.get('/', function (req, res){
-  fs.readFile('views/form.html', function(err, data){
-    res.writeHead(200, {'Content-Type': 'text/html', 'Content-Length': data.length });
-    res.write(data);
-    res.end();
-  });
+  res.render('form');
 });
 
-// POST http://localhost:8080/add
-// parameters sent with
-app.post('/add', function(req, res){
+/* successfully added to db */
+app.post('/success', function(req, res){
   var url = req.body.url;
   var shortLink;
   do {
@@ -27,14 +29,12 @@ app.post('/add', function(req, res){
   var MongoClient = require('mongodb').MongoClient;
   MongoClient.connect("mongodb://localhost:27017/iklico", function(err, db) {
     db.collection("shortlinks").insert({url: url, shortcut: shortLink});
-    console.log('done adding the url ' + shortLink + ' to the db');
   });
 
-  console.log(shortLink);
-  res.render('views/add.html', {shortlink: shortLink});  });
+  res.render('success', {shortlink: shortLink});  });
 
 /* forward to corresponding URL*/
-app.get('/apples/:link', function(req, res){
+app.get('/:link', function(req, res){
   var shortLink = req.params.link;
   var MongoClient = require('mongodb').MongoClient;
 
@@ -58,15 +58,14 @@ app.listen(3000, function(){
 
 function generateLink(url){
   var chars = '';
-  //alpha = "abcdefghjkmnopqrstuvwxyz";
-  alpha = "wxyz";
-  //nums = "23456789"
+  alpha = "abcdefghjkmnopqrstuvwxyz";
+  nums = "23456789"
   for (i = 0; i < 5; i++){
     chars += alpha.charAt(Math.floor(Math.random()*alpha.length));
   }
 
   for (i = 0; i < 2; i++){
-    //chars += nums.charAt(Math.floor(Math.random()*nums.length));
+    chars += nums.charAt(Math.floor(Math.random()*nums.length));
   }
 
   return chars;
